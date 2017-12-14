3bb38f1c1e99ad1a459ef485ea1f6e426af6abfa
Adjamilton Junior <jr@ieee.org>
2017-12-14 13:15:42 -0200

    add bd.sql
    Script SQL para criação do banco de dados.

---
+++ bd/bd.sql
@@ -0,0 +1,7 @@
+-- Script SQL do Banco de Dados do Sistema de Gestão do SECIF.
+-- Copyright (C) 2017-18, Instituto Federal da Paraíba - IFPB.
+--
+-- SGBD: MySQL (v 5.7)
+
+CREATE DATABASE secif_db;
+USE secif_db;
