ec6ae976fb8841d5bcc52d5aeadbdebe1722509e
Rafael Cardoso <rafaelalbuquerquecardoso@gmail.com>
2017-12-29 00:13:47 -0300

    Cadastro de aluno/servidor sendo realizado

--- bd/secif.sql
+++ bd/secif.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 28-Dez-2017 às 16:31
+-- Generation Time: 29-Dez-2017 às 04:10
 -- Versão do servidor: 10.1.21-MariaDB
 -- PHP Version: 5.6.30
 
@@ -20,164 +20,77 @@ SET time_zone = "+00:00";
 -- Database: `secif`
 --
 
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `categoria`
---
-
-CREATE TABLE `categoria` (
-  `id_categoria` int(11) NOT NULL,
-  `nome` varchar(30) NOT NULL,
-  `descricao` varchar(30) NOT NULL
+CREATE TABLE `aluno` (
+  `matricula` varchar(100) NOT NULL,
+  `situacao_periodo` varchar(50) DEFAULT NULL,
+  `id_curso` int(11) NOT NULL,
+  `periodo_ano` varchar(6) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `modalidade`
---
-
-CREATE TABLE `modalidade` (
-  `id_modalidade` int(11) NOT NULL,
-  `nome` varchar(128) NOT NULL,
-  `descricao` varchar(256) DEFAULT NULL,
-  `equipe_quant` int(11) DEFAULT NULL,
-  `categoria` int(11) DEFAULT NULL
+CREATE TABLE `curso` (
+  `id_curso` int(11) NOT NULL,
+  `curso` varchar(255) NOT NULL,
+  `campus` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- --------------------------------------------------------
+CREATE TABLE `funcionario` (
+  `matricula` varchar(100) NOT NULL,
+  `cargo` varchar(70) NOT NULL,
+  `setor` varchar(70) NOT NULL,
+  `tercerizado` tinyint(1) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
---
--- Estrutura da tabela `participacao`
---
-
-CREATE TABLE `participacao` (
-  `id_participacao` int(11) NOT NULL,
-  `participante` int(11) NOT NULL,
-  `modalidade` int(11) NOT NULL,
-  `pontos` int(11) DEFAULT '0'
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `participante`
---
 
 CREATE TABLE `participante` (
-  `matricula` int(6) NOT NULL,
-  `identidade` varchar(26) DEFAULT NULL,
-  `nome` varchar(128) DEFAULT NULL,
-  `nomeArtistico` varchar(128) DEFAULT NULL,
-  `email` varchar(128) DEFAULT NULL,
-  `sexo` varchar(2) DEFAULT NULL,
-  `endereco` varchar(128) DEFAULT NULL,
-  `cidade` varchar(128) DEFAULT NULL,
-  `uf` varchar(2) DEFAULT NULL,
-  `fone` varchar(26) DEFAULT NULL,
-  `cargo` int(11) DEFAULT NULL
+  `matricula` varchar(100) NOT NULL,
+  `nome` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `sexo` char(2) NOT NULL,
+  `endereco` varchar(255) NOT NULL,
+  `cidade` varchar(128) NOT NULL,
+  `fone` varchar(40) DEFAULT NULL,
+  `senha` longtext NOT NULL,
+  `data_nascimento` date NOT NULL,
+  `complemento` varchar(255) DEFAULT NULL,
+  `bairro` varchar(255) NOT NULL,
+  `uf` varchar(2) NOT NULL,
+  `cep` int(10) NOT NULL,
+  `tamanho_camisa` varchar(4) NOT NULL,
+  `atestado` int(1) NOT NULL,
+  `lista_email` int(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- --------------------------------------------------------
+
+ALTER TABLE `aluno`
+  ADD KEY `fk_participante2` (`matricula`);
 
 --
--- Estrutura da tabela `tipo_participante`
+-- Indexes for table `curso`
 --
-
-CREATE TABLE `tipo_participante` (
-  `id_tipo` int(11) NOT NULL,
-  `area` varchar(128) NOT NULL,
-  `responsabilidade` varchar(128) NOT NULL,
-  `tipo` varchar(128) NOT NULL,
-  `turno` varchar(26) DEFAULT NULL,
-  `periodo` varchar(128) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+ALTER TABLE `curso`
+  ADD PRIMARY KEY (`id_curso`);
 
 --
--- Indexes for dumped tables
+-- Indexes for table `funcionario`
 --
-
---
--- Indexes for table `categoria`
---
-ALTER TABLE `categoria`
-  ADD PRIMARY KEY (`id_categoria`);
-
---
--- Indexes for table `modalidade`
---
-ALTER TABLE `modalidade`
-  ADD PRIMARY KEY (`id_modalidade`),
-  ADD KEY `categoria` (`categoria`);
-
---
--- Indexes for table `participacao`
---
-ALTER TABLE `participacao`
-  ADD PRIMARY KEY (`id_participacao`),
-  ADD KEY `participante` (`participante`),
-  ADD KEY `modalidade` (`modalidade`);
+ALTER TABLE `funcionario`
+  ADD KEY `fk_participante` (`matricula`);
 
 --
 -- Indexes for table `participante`
 --
 ALTER TABLE `participante`
-  ADD PRIMARY KEY (`matricula`),
-  ADD KEY `cargo` (`cargo`);
+  ADD PRIMARY KEY (`matricula`);
 
---
--- Indexes for table `tipo_participante`
---
-ALTER TABLE `tipo_participante`
-  ADD PRIMARY KEY (`id_tipo`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `categoria`
---
-ALTER TABLE `categoria`
-  MODIFY `id_categoria` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `modalidade`
---
-ALTER TABLE `modalidade`
-  MODIFY `id_modalidade` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `participacao`
---
-ALTER TABLE `participacao`
-  MODIFY `id_participacao` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `tipo_participante`
---
-ALTER TABLE `tipo_participante`
-  MODIFY `id_tipo` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- Constraints for dumped tables
 --
 
 --
--- Limitadores para a tabela `modalidade`
+-- Limitadores para a tabela `aluno`
 --
-ALTER TABLE `modalidade`
-  ADD CONSTRAINT `modalidade_ibfk_1` FOREIGN KEY (`categoria`) REFERENCES `categoria` (`id_categoria`);
-
---
--- Limitadores para a tabela `participacao`
---
-ALTER TABLE `participacao`
-  ADD CONSTRAINT `participacao_ibfk_1` FOREIGN KEY (`participante`) REFERENCES `participante` (`matricula`),
-  ADD CONSTRAINT `participacao_ibfk_2` FOREIGN KEY (`modalidade`) REFERENCES `modalidade` (`id_modalidade`);
-
---
--- Limitadores para a tabela `participante`
---
-ALTER TABLE `participante`
-  ADD CONSTRAINT `participante_ibfk_1` FOREIGN KEY (`cargo`) REFERENCES `tipo_participante` (`id_tipo`);
+ALTER TABLE `aluno`
+  ADD CONSTRAINT `FK_Aluno` FOREIGN KEY (`matricula`) REFERENCES `participante` (`matricula`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
--- server.js
+++ server.js
@@ -25,20 +25,99 @@ app.use('/assets', express.static('assets'));
 
 // Landing page
 app.get('/', function (req, res) { 
-	var connection = mysql.createConnection({
-	  host     : 'localhost',
-	  user     : 'root',
-	  password : ''
-	});
-	
-	connection.connect();
-	
-	connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
-	  if (err) throw err;
-	  console.log('The solution is: ', rows[0].solution);
-	});
-	
 	res.render('pages/index/index'); 
+});
+
+app.post('/registrar', (req, res) =>{
+	const matricula = req.body.matricula;
+	const nome = req.body.nome;
+	const email = req.body.email;
+	const sexo = req.body.sexo;
+	const endereco = req.body.endereco;
+	const cidade = req.body.cidade;
+	const fone = req.body.fone;
+	const senha = req.body.senha;
+	const data_nascimento = req.body.data_nascimento;
+	const complemento = req.body.complemento;
+	const bairro = req.body.bairro;
+	const uf = req.body.uf;
+	const cep = req.body.cep;
+	const tamanho = req.body.tamanho_camisa;
+	const curso = req.body.curso;
+	const atestado = req.body.atestado;
+	const lista_email = req.body.lista_email;
+
+	execSQLQuery(`INSERT INTO participante VALUES('${matricula}',
+	'${nome}',
+	'${email}',
+	'${sexo}',
+	'${endereco}',
+	'${cidade}',
+	'${fone}',
+	'${senha}',
+	'${data_nascimento}',
+	'${complemento}',
+	'${bairro}',
+	'${uf}',
+	'${cep}',
+	'${tamanho}',
+	'${atestado}',
+	'${lista_email}')`, function (error, results) {
+		console.log("Entrou no callback inscricao");
+
+		if (error != null && typeof error != 'undefined') {
+			console.log("erro na query sql erro= "+error);
+		} else {
+			if (results == "" || typeof results == 'undefined') {
+				console.log("a query nao retornou nada");
+			} else {
+				console.log("existem resultados");
+				if (req.body.tipo == 1) {
+					console.log("É aluno");
+					execSQLQuery(`INSERT INTO aluno VALUES('${req.body.matricula}',
+					NULL,
+					'${req.body.curso}',
+					'${req.body.periodo_ano}')`, callbackResult);
+				} else {
+					if (req.body.tipo == 2) {
+						console.log("É servidor do if");
+						var terceirizado = 0;
+					} else if (req.body.tipo == 3) {
+						console.log("É terceirizado");
+						var terceirizado = 1;
+					}
+					execSQLQuery(`INSERT INTO funcionario VALUES('${req.body.matricula}',
+					'${req.body.cargo}',
+					'${req.body.setor}',
+					'${terceirizado}')`, callbackResult);
+				}
+			}
+		}
+	});
+
+	function callbackResult(error, results) {
+		console.log("Entrou no callback result");
+		var msg = "";
+		var sv = "";
+
+		if (error != null && typeof error != 'undefined') {
+			console.log("erro na query sql");
+			msg = "Um problema aconteceu com o banco de dados!";
+			sv = "danger";
+		} else {
+			if (results == "" || typeof results == 'undefined') {
+				console.log("a query nao retornou nada");
+				msg = "Algum erro aconteceu..";
+				sv = "warning";
+			} else {
+				console.log("existem resultados");
+				msg = "Sua inscrição foi realizada com sucesso!";
+				sv = "success";
+			}
+		}
+
+		res.render('pages/index/result', { alert: msg, severity: sv });
+	}
 });
 
 // Blog page
@@ -50,3 +129,21 @@ app.get('/dashboard/login', function (req, res) { res.render('pages/dashboard/in
 app.listen(port, "0.0.0.0", function() {
 	console.log("Listening on Port 5000");
 });
+
+function execSQLQuery(sqlQry, callback){
+  const connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : '',
+    database : 'secif'
+  });
+ 
+  connection.query(sqlQry, function(error, results, fields){
+	  if(error) 
+	  	callback(error, null);
+      else
+        callback(null, results);
+      connection.end();
+      console.log('executou!');
+  });
+}
